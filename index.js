767618cb7dd798c34845e105ea50c278162fab00
matthew rendely <matthew.rendely@gmail.com>
2023-02-18 12:25:14 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, giftType){
+  const thankYous = [];
+  for (let i = 0; i < names.length; i++){
+    thankYous.push(`Thank you, ${names[i]}, for the wonderful ${giftType} gift!`);
+  }
+  return thankYous;
+}
+
+function countDown(n){
+  while (n >= 0){
+    console.log(n);
+    n--;
+  }
+}
